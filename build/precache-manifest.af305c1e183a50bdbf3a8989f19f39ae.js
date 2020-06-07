3c6691e48315e25f472e59249a5846b08536c199
Ayushya Jaiswal <ayushya@instamojo.com>
2020-06-07 22:43:51 +0530

    Push build to github

---
+++ build/precache-manifest.af305c1e183a50bdbf3a8989f19f39ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dfa178c3407aa46af9259550d81de4e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "505924237f2126084d00",
+    "url": "/static/css/main.42fc4135.chunk.css"
+  },
+  {
+    "revision": "9f4f615c5c8dc031f55b",
+    "url": "/static/js/2.c9aba62e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.c9aba62e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "505924237f2126084d00",
+    "url": "/static/js/main.5a96f46c.chunk.js"
+  },
+  {
+    "revision": "79a212563cc49c188366",
+    "url": "/static/js/runtime-main.7e5024e5.js"
+  },
+  {
+    "revision": "841cd05159720005cb8c64cc2f421015",
+    "url": "/static/media/back.841cd051.svg"
+  }
+]);
