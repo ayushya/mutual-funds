8a88ab2d1801d0b608160e648f017860f17654b9
Ayushya Jaiswal <ayushya.jaiswal@uipath.com>
2023-12-08 23:00:15 +0530

    Added cors first

--- server/server.ts
+++ server/server.ts
@@ -1,9 +1,11 @@
 import bodyParser from 'body-parser';
 import compression from 'compression';
+import cors from 'cors';
 import helmet from 'helmet';
 
 import app from './app';
 
+app.use(cors());
 // Configure body-parser to handle JSON data
 app.use(bodyParser.json());
 app.use(helmet()); // set well-known security-related HTTP headers
@@ -11,7 +13,7 @@ app.use(compression());
 
 app.disable("x-powered-by");
 
-const port = 3000;
+const port = 4000;
 
 const server = app.listen(port, () =>
     console.log("Starting ExpressJS server on Port " + port));
