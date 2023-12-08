fdb709bffb057f6f82d361b4be81b887f81cb6e4
Ayushya Jaiswal <ayushya.jaiswal@uipath.com>
2023-12-08 22:41:14 +0530

    Added cache in server

--- server/app.ts
+++ server/app.ts
@@ -2,43 +2,38 @@ import axios from 'axios';
 import deepmerge from 'deepmerge';
 import express from 'express';
 
-import { GET_FUNDS } from './constants';
+import {
+  DEFAULT_DURATION,
+  GET_FUNDS,
+} from './constants';
+import { formatData } from './utility';
 
-const app = express()
+const app = express();
 
+export const CACHE_EXPIRY = 60 * 60 * 1000; // 60 Mins
+
+let dataCache = null;
+
+// Reset dataCache with every CACHE_EXPIRY time
+setInterval(() => {
+  dataCache = null;
+}, CACHE_EXPIRY);
 
 app.get('/', (req, res) => {
         res.redirect('/healthcheck');
     });
   
-  app.get('/getFunds', async (req, res) => {
-    // const data = await fetch("https://api.kuvera.in/insight/api/v1/mutual_fund_search.json?limit=10000&scheme_plan=GROWTH&order_by=desc&v=342", {
-    //   "headers": {
-    //     "accept": "application/json, text/plain, */*",
-    //     "accept-language": "en-IN,en-US;q=0.9,en-GB;q=0.8,en;q=0.7",
-    //     "cache-control": "no-cache",
-    //     "pragma": "no-cache",
-    //     "sec-ch-ua": "\"Not_A Brand\";v=\"8\", \"Chromium\";v=\"120\", \"Google Chrome\";v=\"120\"",
-    //     "sec-ch-ua-mobile": "?0",
-    //     "sec-ch-ua-platform": "\"macOS\"",
-    //     "sec-fetch-dest": "empty",
-    //     "sec-fetch-mode": "cors",
-    //     "sec-fetch-site": "cross-site",
-    //     "Referer": "https://ayushya.github.io/",
-    //     "Referrer-Policy": "strict-origin-when-cross-origin"
-    //   },
-    //   "body": null,
-    //   "method": "GET"
-    // });
+  app.get('/getFunds', async (req, res) => {  
+    if (!dataCache) {
+      const growth = await axios.get(GET_FUNDS('GROWTH')).then(r => r.data);
+      const dividend = await axios.get(GET_FUNDS('DIVIDEND')).then(r => r.data);
+      const rawData: any = deepmerge(growth, dividend);
   
-  
-    const growth = await axios.get(GET_FUNDS('GROWTH')).then(r => r.data);
-    const dividend = await axios.get(GET_FUNDS('DIVIDEND')).then(r => r.data);
-    const data: any = deepmerge(growth, dividend);
-  
-  
-    // console.log(data.data);
-    res.json(data.data);
+      const data = await formatData(rawData, DEFAULT_DURATION);
+      dataCache = data;
+    }
+
+    res.json(dataCache);
   });
   
   app.get('/healthcheck', (req, res) => {
---
+++ server/utility.ts
@@ -0,0 +1,80 @@
+import axios from 'axios';
+
+import { GET_FUNDS_DETAILS } from './constants';
+
+export const formatData = async (data, duration) => {
+    let fundList: any = [];
+
+    data.data.funds.forEach(fund => {
+        const {name, sub_category: subCategory, category, unique_fund_code: code, one_day_return } = fund;
+        fundList.push({
+            name,
+            code,
+            category,
+            subCategory,
+            one_day_return,
+        });
+    });
+
+    // const categoryList = Object.keys(data);
+    // categoryList?.forEach((category) => {
+    //     const subCategoryList = Object.keys(data[category]);
+    //     subCategoryList.forEach((subCategory) => {
+    //         const fundHouseList = Object.keys(data[category][subCategory]);
+    //         fundHouseList.forEach((fundHouse) => {
+    //             data[category][subCategory][fundHouse].forEach((fund) => {
+    //                 const {
+    //                     c,
+    //                     kc,
+    //                     n,
+    //                     r,
+    //                     // re,
+    //                     v,
+    //                 } = fund;
+    //                 fundList.push({
+    //                     name: n,
+    //                     code: c,
+    //                     returns: r,
+    //                     volatility: v,
+    //                     categoryMentioned: kc,
+    //                     category,
+    //                     subCategory,
+    //                 });
+    //             });
+    //         });
+    //     });
+    // });
+
+    const DATA_BATCH_SIZE = 50;
+    const NUMBER_OF_API_CALLS = Math.ceil(fundList.length / DATA_BATCH_SIZE);
+
+    const promiseList: any = [];
+    for (let i = 0; i < NUMBER_OF_API_CALLS; i++) {
+        const fundCodeListString = fundList
+            .slice(i * DATA_BATCH_SIZE, i * DATA_BATCH_SIZE + DATA_BATCH_SIZE)
+            .reduce(
+                (prev, curr) => (prev ? `${prev}|${curr.code}` : curr.code),
+                ""
+            );
+        const apiPromise = axios
+            .get(GET_FUNDS_DETAILS(fundCodeListString))
+            .then((response) => {
+                const { data } = response;
+                data.forEach((details, index) => {
+                    fundList[i * DATA_BATCH_SIZE + index].details = details;
+                });
+            });
+        promiseList.push(apiPromise);
+    }
+    await Promise.all(promiseList);
+
+    // Filter all the funds which are unavailable
+    return fundList.filter(
+        (fund) =>
+            !(
+                fund?.details?.sip_available === "N" &&
+                fund?.details?.lump_available === "N" &&
+                fund?.details?.redemption_allowed === "N"
+            )
+    );
+};
